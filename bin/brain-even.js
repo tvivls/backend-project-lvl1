ad719d10a61ee9c1eb905d2619963c87d42a2bf4
Valentina <valya.cherednichenko.03@mail.ru>
2022-10-08 22:42:07 +0300

    srt writes right answer

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -7,23 +7,18 @@ const name = greetingByName();
 
 const isEvenNumber = (name) => {
     console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    let number;
-    const rightAnsw = ['yes', 'no'];
+    let number, rightAnsw;
+    rightAnsw = (even) ? 'yes' : 'no';
     for (let i = 0; i < 3; i += 1) {
         number = getRandomInt(0, 100);
         console.log('Question: ', number);
         let answer = readlineSync.question('Your answer(yes/no): ');
-        if (even(number) && answer === rightAnsw[0]) {
-            console.log('Correct!');
-            continue;
-        }
-        if (!even(number) && answer === rightAnsw[1]) {
+        if (answer === rightAnsw) {
             console.log('Correct!');
             continue;
         }
         else {
-            let str = `'${answer}' is wrong answer ;(. Correct answer was `;
-            str += (answer !== rightAnsw[0]) ? `'${rightAnsw[0]}'.` : `'${rightAnsw[1]}'.`;
+            let str = `'${answer}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`;
             console.log(str);
             console.log(`Let's try again, ${name}!`);
             return false;
