a22b5ec2d60d239c6fa8463bd116441099aba9e3
Valentina <valya.cherednichenko.03@mail.ru>
2022-10-08 21:56:24 +0300

    add calculations

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -7,15 +7,38 @@ const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
 
 const calculator = (name) => {
     console.log('What is the result of the expression?');
-    let number1, number2, numbOfOperator;
+    let number1, number2, numbOfOperator, rightAnsw;
     const operator = ['+', '-', '*'];
     for (let i = 0; i < 3; i += 1) {
         number1 = getRandomInt(0, 100);
-        number1 = getRandomInt(0, 100);
+        number2 = getRandomInt(0, 100);
         numbOfOperator = getRandomInt(0, 3);
+        
         console.log('Question: ', number1, operator[numbOfOperator], number2);
         let answer = readlineSync.question('Your answer: ');
-
+        switch(numbOfOperator){
+            case 0:
+                rightAnsw = number1 + number2;
+                break;
+            case 1:
+                rightAnsw = number1 - number2;
+                break;
+            case 2:
+                rightAnsw = number1 * number2;
+                break;
+        }
+        if (rightAnsw === Number(answer)) {
+            console.log('Correct!');
+            continue;
+        }
+        if (rightAnsw !== answer) {
+            let str = `'${answer}' is wrong answer ;(. Correct answer was `;
+            
+            console.log(str);
+            console.log(`Let's try again, ${name}!`);
+            calculator(name);
+        }
     }
     console.log(`Congratulations, ${name}!`);
 };
+calculator(name);
