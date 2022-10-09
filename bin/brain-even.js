84c06c89aa264777158c9a5fdc8d955b92f28d66
Valentina <valya.cherednichenko.03@mail.ru>
2022-10-09 15:46:07 +0300

    transfer game to games/even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,31 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetingByName from '../src/cli.js';
+import isEvenNumber from '../games/even.js';
 
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const even = (number) => number % 2 === 0;
-const name = greetingByName();
-
-const isEvenNumber = (name) => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    let number, rightAnsw;
-    for (let i = 0; i < 3; i += 1) {
-        number = getRandomInt(0, 100);
-        rightAnsw = (even(number)) ? 'yes' : 'no';
-        console.log('Question:', number);
-        let answer = readlineSync.question('Your answer(yes/no): ');
-        if (answer === rightAnsw) {
-            console.log('Correct!');
-            continue;
-        }
-        else {
-            let str = `'${answer}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`;
-            console.log(str);
-            console.log(`Let's try again, ${name}!`);
-            return false;
-        }
-    }
-    console.log(`Congratulations, ${name}!`);
-}
-export default isEvenNumber;
-isEvenNumber(name);
+isEvenNumber();
---
+++ games/even.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+import greetingByName from '../src/cli.js';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+const isEven = (number) => number % 2 === 0;
+const name = greetingByName();
+
+const isEvenNumber = () => {
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    let number, rightAnsw;
+    for (let i = 0; i < 3; i += 1) {
+        number = getRandomInt(0, 100);
+        rightAnsw = (isEven(number)) ? 'yes' : 'no';
+        console.log('Question:', number);
+        let answer = readlineSync.question('Your answer(yes/no): ');
+        if (answer === rightAnsw) {
+            console.log('Correct!');
+            continue;
+        }
+        else {
+            let str = `'${answer}' is wrong answer ;(. Correct answer was '${rightAnsw}'.`;
+            console.log(str);
+            console.log(`Let's try again, ${name}!`);
+            return false;
+        }
+    }
+    console.log(`Congratulations, ${name}!`);
+};
+export default isEvenNumber;
