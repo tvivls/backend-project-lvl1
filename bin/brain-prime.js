c7fe23831a628ad97d0eb449566d48983b639ff1
Valentina <valya.cherednichenko.03@mail.ru>
2022-10-24 01:44:45 +0300

    correct call

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import primeNumber from '../games/prime.js';
 
 primeNumber();
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import arithmeticProgression from '../games/progression.js';
 
 arithmeticProgression();
